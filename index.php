f1d9b76b75f53e2c21a1a69efbc97a4ec2b4f4fe
Rudie Dirkx <rdirkx@pronexus.nl>
2017-12-12 14:33:30 +0100

    custom function "contains(haystack, needle)", but no custom operator "haystack contains needle"

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ use Symfony\Component\ExpressionLanguage\ExpressionLanguage;
 
 require 'vendor/autoload.php';
 
+ini_set('html_errors', 0);
 header('Content-type: text/plain; charset=utf-8');
 
 class User {
@@ -29,8 +30,27 @@ class User {
 	function get_num_permissions() {
 		return count($this->permissions);
 	}
+	function get_group() {
+		return new Group('Whatevers', new Source(!$this->admin));
+	}
 	function __toString() {
 		return $this->name;
+	}
+}
+
+class Group {
+	public $name;
+	public $source;
+	function __construct($name, Source $source) {
+		$this->name = $name;
+		$this->source = $source;
+	}
+}
+
+class Source {
+	public $editable;
+	function __construct($editable) {
+		$this->editable = $editable;
 	}
 }
 
@@ -41,16 +61,54 @@ echo "\n";
 
 $_time = microtime(1);
 
-$expression = 'user.admin or (user.num_permissions > 0 and user.num_permissions < 3243 and user.age in ages)';
+$expression = '
+	user.group.name matches "/^[A-Z]/" and
+	(
+		user.group.source.editable or
+		user.admin or
+		(user.num_permissions > 0 and user.num_permissions < 3243 and user.age in ages)
+	)
+';
 $params = [
 	'user' => $user,
 	'ages' => [10, 15, 20],
 ];
 
 $language = new ExpressionLanguage();
+
 var_dump($language->evaluate($expression, $params));
 
 echo "\n" . number_format((microtime(1) - $_time) * 1000, 1) . " ms (with 100 ms sleep?)\n\n";
+$_time = microtime(1);
 
 // print_r($language->parse($expression, array_keys($params)));
+var_dump($language->compile($expression, array_keys($params)));
+
+echo "\n" . number_format((microtime(1) - $_time) * 1000, 1) . " ms (with 100 ms sleep?)\n\n";
+
+echo "\n\n\n\n\n\n";
+
+
+
+//$language = new ExpressionLanguage();
+//
+//$expression = 'x contains y';
+//$params = ['x' => "James O'brien", 'y' => "'"];
+//var_dump($language->evaluate($expression, $params));
+//var_dump($language->compile($expression, array_keys($params)));
+
+//echo "\n\n\n\n\n\n";
+
+
+
+$language = new ExpressionLanguage();
+$language->register('contains', function($haystack, $needle) {
+    return sprintf('is_int(strpos(%s, %s))', $haystack, $needle);
+}, function($args, $haystack, $needle) {
+    return is_int(strpos($haystack, $needle));
+});
+
+$expression = 'contains(x, y) or contains(y, x)';
+$params = ['x' => "abc", 'y' => "b"];
+var_dump($language->evaluate($expression, $params));
 var_dump($language->compile($expression, array_keys($params)));
